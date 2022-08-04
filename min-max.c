301a61ad0c7279bd79214ed9895ee3133ca487a4
Dibyan Neupane <89589920+dibbbbbbss@users.noreply.github.com>
2022-08-04 22:28:08 +0545

    created a min max program

---
+++ min-max.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+    printf("enter two numbers \n");
+    int a;
+    int b;
+    scanf("%d",&a );
+    scanf("%d",&b );
+
+    if(a>b) {
+        printf("%d is the max number, and %d is the min number",a,b);
+    } else if(b>a) {
+        printf(" %d is the max number , and %d is the min number",b,a);
+
+    } else {
+        printf("error");
+    }
+
+
+    return 0;
+}
